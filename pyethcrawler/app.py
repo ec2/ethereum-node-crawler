0eb157555ee9403e7d696efa4b41a5513933c617
Simon Kim <skim104@illinois.edu>
2017-01-19 01:24:16 +0000

    peer connecting loop

--- pyethcrawler/app.py
+++ pyethcrawler/app.py
@@ -19,12 +19,10 @@ from devp2p import slogging
 from devp2p import utils
 from devp2p.app import BaseApp
 from devp2p.discovery import NodeDiscovery
-from devp2p.peer import Peer
 from devp2p.protocol import BaseProtocol
 from devp2p.p2p_protocol import P2PProtocol
-from devp2p.service import BaseService
-from peermanager2 import PeerManager
-from testcrawler import ChainService
+from devp2p.service import BaseService, WiredService
+from peercrawler import PeerCrawler
 
 log = slogging.get_logger('app')
 slogging.configure(config_string=':debug')
@@ -34,8 +32,38 @@ if root_logger.handlers == []:
     root_logger.addHandler(slogging.logging.StreamHandler(sys.stderr))
 #log = slogging.get_logger('p2p')
 
+
+class ETHProtocol(BaseProtocol):
+    protocol_id = 1
+    network_id = 1
+    max_cmd_id = 17  # FIXME
+    name = 'eth'
+    version = 63
+
+    max_getblocks_count = 64
+    max_getblockhashes_count = 2048
+
+    def __init__(self, version):
+        if version in [61, 62]:
+            self.max_cmd_id = 8
+            self.version = version
+
+
+class ChainService(WiredService):
+    # required by BaseService
+    name = 'chain'
+
+    # required by WiredService
+    wire_protocol = ETHProtocol
+
+    def __init__(self, version):
+        if version in [61, 62]:
+            self.wire_protocol.max_cmd_id = 8
+            self.wire_protocol.version = version
+
+
 class BaseApp(object):
-    client_name = 'pyethapp-test'
+    client_name = 'pyethcrawler'
     client_version = '%s/%s/%s' % (__version__, sys.platform,
                                    'py%d.%d.%d' % sys.version_info[:3])
     client_version_string = '%s/v%s' % (client_name, client_version)
@@ -43,9 +71,9 @@ class BaseApp(object):
     default_config = dict(BaseApp.default_config)
     default_config['client_version_string'] = client_version_string
     default_config['result_dir'] = ''
-    default_config['prev_routing_table'] = ''
+    default_config['prev_routing_table'] = '/results/routing-table_170109-084715.csv'
     default_config['prev_peers'] = ''
-    default_config['p2p'] = dict(bootstrap_nodes=[],listen_port=30303,listen_host='0.0.0.0')
+#    default_config['p2p'] = dict(bootstrap_nodes=[],listen_port=30304,listen_host='0.0.0.0')
     privkey_hex = 'b3b9736ba5e4b9d0f85231291316c7fd82bde4ae80bb7ca98175cf6d11c0c4eb'
     pubkey = crypto.privtopub(privkey_hex.decode('hex'))
     default_config['node'] = dict(privkey_hex=privkey_hex, id=crypto.sha3(pubkey))
@@ -93,13 +121,16 @@ def main():
 
     # register services
     # NodeDiscovery.register_with_app(app)
-    PeerManager.register_with_app(app)
+    PeerCrawler.register_with_app(app)
     print(app.services)
 
     # start app
     app.start()
 
-    app.services['eth'] = ChainService()
+#    app.services['eth61'] = ChainService(61)
+#    app.services['eth62'] = ChainService(62)
+#    app.services['eth63'] = ChainService(63)
+
     # wait for interupt
     evt = gevent.event.Event()
     # gevent.signal(signal.SIGQUIT, gevent.kill) ## killall pattern
---
+++ pyethcrawler/peercrawler.py
@@ -0,0 +1,163 @@
+import random
+import gevent
+import socket
+import atexit
+import time
+import csv
+from gevent.server import StreamServer
+from gevent.socket import create_connection, timeout
+from devp2p import multiplexer
+from devp2p import rlpxcipher
+from devp2p.service import WiredService
+from devp2p.protocol import BaseProtocol
+from devp2p.p2p_protocol import P2PProtocol
+from devp2p import kademlia
+from devp2p.peer import *
+from devp2p.peermanager import *
+from devp2p import crypto
+from devp2p.crypto import ECIESDecryptionError
+from devp2p import utils
+
+from devp2p import slogging
+log = slogging.get_logger('p2p.peermgr')
+
+class NewPeer(Peer):
+    @property
+    def capabilities(self):
+        return [('eth', 61), ('eth', 62), ('eth', 63), ('p2p', 4)]
+
+    def _handle_packet(self, packet):
+        assert isinstance(packet, multiplexer.Packet)
+        try:
+            protocol, cmd_id = self.protocol_cmd_id_from_packet(packet)
+            cmd = protocol.cmd_by_id[cmd_id]
+        except (UnknownCommandError, KeyError) as e:
+            log.error('received unknown cmd', error=e, packet=packet)
+            return
+        log.debug('recv packet ', cmd=cmd, protocol=protocol.name, orig_cmd_id=packet.cmd_id)
+        packet.cmd_id = cmd_id  # rewrite
+        protocol.receive_packet(packet)
+
+    def _run_ingress_message(self):
+        log.debug('wtfwtfpeer starting main loop')
+        assert not self.connection.closed, "connection is closed"
+        gevent.spawn(self._run_decoded_packets)
+        gevent.spawn(self._run_egress_message)
+
+        while not self.is_stopped:
+            self.safe_to_read.wait()
+            try:
+                gevent.socket.wait_read(self.connection.fileno())
+            except gevent.socket.error as e:
+                log.debug('read error', errno=e.errno, reason=e.strerror, peer=self)
+                self.report_error('network error %s' % e.strerror)
+                if e.errno in(9,):
+                    # ('Bad file descriptor')
+                    self.stop()
+                else:
+                    raise e
+                    break
+            try:
+                imsg = self.connection.recv(4096)
+            except gevent.socket.error as e:
+                log.debug('read error', errno=e.errno, reason=e.strerror, peer=self)
+                self.report_error('network error %s' % e.strerror)
+                if e.errno in(50, 54, 60, 65, 104):
+                    # (Network down, Connection reset by peer, timeout, no route to host,
+                    # Connection reset by peer)
+                    self.stop()
+                else:
+                    raise e
+                    break
+            if imsg and (len(imsg) >= 210):
+                log.debug('read data', ts=time.time(), size=len(imsg))
+                try:
+                    self.mux.add_message(imsg)
+                except (rlpxcipher.RLPxSessionError, ECIESDecryptionError) as e:
+                    log.debug('rlpx session error', peer=self, error=e)
+                    self.report_error('rlpx session error')
+                    self.stop()
+                except multiplexer.MultiplexerError as e:
+                    log.debug('multiplexer error', peer=self, error=e)
+                    self.report_error('multiplexer error')
+                    self.stop()
+
+    _run = _run_ingress_message
+
+
+class PeerCrawler(PeerManager):
+    name = 'peercrawler'
+    required_services = []
+    wire_protocol = P2PProtocol
+    default_config = dict(p2p=dict(listen_port=30303,listen_host='0.0.0.0'),
+                          log_disconnects=True)
+
+    connect_timeout = 2.
+    connect_loop_delay = 0.1
+    discovery_delay = 0.5
+
+    def __init__(self, app):
+        log.info('PeerCrawler init')
+        WiredService.__init__(self, app)
+        self.peers = []
+        self.errors = PeerErrors() if self.config['log_disconnects'] else PeerErrorsBase()
+
+    def on_hello_received(self, proto, version, client_version_string, capabilities,
+                          listen_port, remote_pubkey):
+        log.debug('hello_received', peer=proto.peer, num_peers=len(self.peers))
+        if remote_pubkey in [p.remote_pubkey for p in self.peers if p != proto.peer]:
+            log.debug('connected to that node already')
+            proto.send_disconnect(proto.disconnect.reason.useless_peer)
+            return False
+
+        return True
+
+    def _start_peer(self, connection, address, remote_pubkey=None):
+        # create peer
+        peer = NewPeer(self, connection, remote_pubkey=remote_pubkey)
+        log.debug('created new peer', peer=peer, fno=connection.fileno())
+        self.peers.append(peer)
+
+        # loop
+        peer.start()
+        log.debug('peer started', peer=peer, fno=connection.fileno())
+        assert not connection.closed
+        return peer
+
+    def start(self):
+        log.info('starting peercrawler')
+        gevent.spawn(self._crawl_loop)
+
+    def _crawl_loop(self):
+        nodes = []
+        filename = self.config['prev_routing_table']
+        log.info('loading known nodes from ', filename=filename)
+        file_obj = open(filename, 'r')
+        greenlets = []
+        csvfile = csv.reader(file_obj, delimiter=',')
+        for row in csvfile:
+            if len(row) != 6:
+                continue
+            pubkey = row[0].decode('hex')
+            nodes.append((pubkey, row[1], int(row[2])))
+        csvfile = None
+        file_obj.close()
+
+        s = 0
+
+#        while not self.is_stopped:
+        while s != 3:
+            for pubkey, ip, tcp_port in nodes:
+                greenlets.append(gevent.spawn(self.connect, (ip, tcp_port), pubkey))
+            gevent.joinall(greenlets)
+            gevent.sleep(self.connect_loop_delay)
+            log.info('============== ', peers=self.num_peers())
+            s += 1
+        evt = gevent.event.Event()
+        evt.wait()
+
+    def stop(self):
+        log.info('stopping peercrawler')
+        for peer in self.peers:
+            peer.stop()
+        super(PeerManager, self).stop()
